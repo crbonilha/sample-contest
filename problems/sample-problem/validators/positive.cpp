e05b1bb011b4fd58928bf753e06d2c14cddf7f9b
Cristhian Bonilha <cristhian.bonilha@hotmail.com>
2021-01-02 10:54:31 +0100

    adding the first validator

---
+++ problems/sample-problem/validators/positive.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+
+	if(a <= 0) {
+		printf("A is not positive: %d\n", a);
+	}
+	if(b <= 0) {
+		printf("B is not positive: %d\n", b);
+	}
+}
+
